81fcecbe0e2e20e50d7d8584bc896a79af38e3fc
denis8sw <denisappleid12@gmail.com>
2024-04-11 17:08:57 +0300

    Input-Icon Component Style & Glitch Fix

--- thatsme/app/global-components/input-icon/InputIcon.tsx
+++ thatsme/app/global-components/input-icon/InputIcon.tsx
@@ -1,52 +1,50 @@
-import React, { ReactNode } from 'react'
-import styles from './page.module.css'
+import React, { ReactNode } from 'react';
+import styles from './page.module.css'; // Assuming you have defined styles for your component
+import './page.module.css'; // This line seems unnecessary
 
-interface componentStyles {
-    backColor: string
-    width: string
-    height: string
-    gap?: string
-    borderRadius: string
-    children: ReactNode
-    fontSize?: string
-    color: string
-    iconBackColor?: string
-    iconBorderRadius?: string
-    title: string
-    titleColor: string
-    titleFontSize?: string
-    type: string
+interface InputIconProps {
+    backColor: string;
+    width: string;
+    height: string;
+    borderRadius: string;
+    children: ReactNode;
+    color: string;
+    fontSize?: string;
+    iconBackColor?: string;
+    iconBorderRadius?: string;
+    title: string;
+    titleColor: string;
+    titleFontSize?: string;
+    type: string;
 }
 
-
 export default function InputIcon({
-    backColor, width, height, gap, borderRadius, children, color,
-    fontSize, iconBackColor, iconBorderRadius, title, titleColor, titleFontSize, type }: componentStyles) {
+    backColor, width, height, borderRadius, children, color,
+    fontSize, iconBackColor, iconBorderRadius, title, titleColor, titleFontSize, type
+}: InputIconProps) {
     return (
         <div className={styles.inputicon}
             style={{
                 backgroundColor: backColor,
                 width: width,
                 height: height,
-                gap: gap,
                 borderRadius: borderRadius
-            }} >
+            }}>
             <span className={styles.icon}
                 style={{
                     color: color,
-                    fontSize: fontSize,
                     backgroundColor: iconBackColor,
                     borderRadius: iconBorderRadius
                 }}>
                 {children}
-                <input className={styles.theinput}
-                    type={type}
-                    placeholder={title}
-                    style={{
-                        color: titleColor,
-                        fontSize: titleFontSize
-                    }}></input>
             </span>
+            <input className={styles.theinput}
+                type={type}
+                placeholder={title}
+                style={{
+                    color: titleColor,
+                    fontSize: fontSize,
+                }} />
         </div>
-    )
+    );
 }
--- thatsme/app/page.tsx
+++ thatsme/app/page.tsx
@@ -3,11 +3,14 @@ import styles from "./page.module.css";
 
 //components
 import InputIcon from "./global-components/input-icon/InputIcon";
+import ButtonIcon from "./global-components/button-icon/buttonicon";
+
+//icons
 import { FaUserAlt } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
+
+
 export default function Home() {
-
-
-
 
   return (
     <>
@@ -15,8 +18,8 @@ export default function Home() {
         <div className={styles.maininputs}>
           <InputIcon
             backColor="white"
-            borderRadius="0.5em"
-            width="250px"
+            borderRadius="0.3em"
+            width="360px"
             height="35px"
             color="black"
             title="Username"
@@ -24,6 +27,19 @@ export default function Home() {
             type="text"
           >
             <FaUserAlt />
+          </InputIcon>
+
+          <InputIcon
+            backColor="white"
+            borderRadius="0.3em"
+            width="360px"
+            height="35px"
+            color="black"
+            title="Password"
+            titleColor="black"
+            type="password"
+          >
+            <FaLock />
           </InputIcon>
         </div>
       </div>
