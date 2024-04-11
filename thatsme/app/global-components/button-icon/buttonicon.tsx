0237e85d37ceae9ae3bb6097efdbbeb3bb45954c
denis8sw <denisappleid12@gmail.com>
2024-04-11 17:50:50 +0300

    Updates buttonicon component

--- thatsme/app/global-components/button-icon/buttonicon.tsx
+++ thatsme/app/global-components/button-icon/buttonicon.tsx
@@ -1,8 +1,9 @@
 import React, { ReactNode } from 'react'
 import styles from './page.module.css'
 
+
 interface ButtonIconProps {
-    backColor: string;
+    backColor?: string;
     width: string;
     height: string;
     borderRadius: string;
@@ -11,30 +12,36 @@ interface ButtonIconProps {
     fontSize?: string;
     iconBackColor?: string;
     iconBorderRadius?: string;
+    iconFontSize: string;
     title: string;
     titleColor: string;
     titleFontSize?: string;
     gap?: string;
+    backgroundImage?: string;
+    transform?: boolean;
 }
 
 export default function ButtonIcon({ backColor, width, height, borderRadius, color, children,
-    iconBackColor, iconBorderRadius, title, titleColor, titleFontSize, gap }: ButtonIconProps) {
+    iconBackColor, iconBorderRadius, title, titleColor, titleFontSize, gap, iconFontSize, backgroundImage, transform }: ButtonIconProps) {
     return (
         <div className={styles.buttonicon} style={{
             backgroundColor: backColor,
             width: width,
             height: height,
             borderRadius: borderRadius,
-            gap: gap
+            gap: gap,
+            backgroundImage: backgroundImage,
         }}>
             <span className={styles.icon} style={{
                 backgroundColor: iconBackColor,
                 borderRadius: iconBorderRadius,
-                color: color
+                color: color,
+                fontSize: iconFontSize
             }}>{children}</span>
             <p className={styles.title} style={{
                 color: titleColor,
-                fontSize: titleFontSize
+                fontSize: titleFontSize,
+                textTransform: transform ? 'uppercase' : 'none'
             }}>{title}</p>
         </div>
     )
