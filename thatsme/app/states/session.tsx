f72d197cce66ba09977ef80ee929a0bdfd11d5f0
denis8sw <denisappleid12@gmail.com>
2024-04-14 21:02:28 +0300

    Creating sessionUsername for the future front end auth

--- thatsme/app/states/session.tsx
+++ thatsme/app/states/session.tsx
@@ -1,13 +1,15 @@
 'use client'
 
 
-import React, { createContext, useContext, useRef, ReactNode } from 'react';
+import React, { createContext, useContext, useRef, ReactNode, MutableRefObject } from 'react';
+import { useRouter } from 'next/navigation'
 import { useError } from './errorstate';
 import { useApiUrl } from './api';
 
 interface FetchContextType {
     checkSession: () => Promise<any>;
     setSession: (value: boolean) => void;
+    sessionUsername: MutableRefObject<string>;
 }
 
 const FetchContext = createContext<FetchContextType | undefined>(undefined);
@@ -26,14 +28,19 @@ interface FetchProviderProps {
 
 export const FetchProvider: React.FC<FetchProviderProps> = ({ children }) => {
 
+    const router = useRouter()
+
     const { showSuccess } = useError()
     const apiUrl = useApiUrl()
 
     const sessionRef = useRef<boolean>(false);
+    const sessionUsername = useRef<string>('')
 
     const setSession = (value: boolean) => {
         sessionRef.current = value;
     }
+
+
     const checkSession = async () => {
         if (localStorage.getItem('token')) {
             try {
@@ -52,6 +59,7 @@ export const FetchProvider: React.FC<FetchProviderProps> = ({ children }) => {
                 if (data.success) {
                     showSuccess(data.success);
                     setSession(true);
+                    getUsername()
                 }
             } catch (error) {
                 setSession(false);
@@ -60,7 +68,32 @@ export const FetchProvider: React.FC<FetchProviderProps> = ({ children }) => {
         }
     };
 
-    const value: FetchContextType = { checkSession, setSession };
+    const getUsername = async () => {
+        try {
+            const response = await fetch(apiUrl + '/getUsername', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ token: localStorage.getItem('token') })
+            });
+            const data = await response.json();
+
+            if (data.error) {
+                setSession(false);
+            }
+            if (data.success) {
+                sessionUsername.current = data.username
+                setTimeout(() => {
+                    router.push('/' + data.username)
+                }, 1700);
+            }
+        } catch (error) {
+           console.log(error)
+        }
+    };
+
+    const value: FetchContextType = { checkSession, setSession, sessionUsername };
 
     return (
         <FetchContext.Provider value={value}>
