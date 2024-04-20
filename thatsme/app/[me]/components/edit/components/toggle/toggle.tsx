7b7b036218351001cc9fa2de05dfe71febec0174
denis8sw <denisappleid12@gmail.com>
2024-04-20 15:04:50 +0300

    Toggle Component created

---
+++ thatsme/app/[me]/components/edit/components/toggle/toggle.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './page.module.css'
+
+export default function Toggle() {
+    return (
+        <div className={styles.toggle}>
+        </div>
+    )
+}
