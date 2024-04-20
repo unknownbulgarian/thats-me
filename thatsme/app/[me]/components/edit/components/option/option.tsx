e23461175583a9f245b24202a58b402d4c91e5c3
denis8sw <denisappleid12@gmail.com>
2024-04-20 15:28:04 +0300

    Adding background color & onclick for the Option component

--- thatsme/app/[me]/components/edit/components/option/option.tsx
+++ thatsme/app/[me]/components/edit/components/option/option.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { MouseEventHandler } from 'react'
 import styles from './page.module.css'
 
 interface OptionProps {
     title: string;
+    backColor?: string;
+    onClick: MouseEventHandler<HTMLDivElement>;
 }
 
-export default function Option({ title, }: OptionProps) {
+export default function Option({ title, backColor, onClick }: OptionProps) {
     return (
-        <div className={styles.option}>
+        <div onClick={onClick} style={{background: backColor}} className={styles.option}>
             <p className={styles.title}>{title}</p>
         </div>
     )
