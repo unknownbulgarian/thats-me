a43a4462e36ac37013ce82ab68fad976ed8f5cb6
denis8sw <denisappleid12@gmail.com>
2024-04-20 15:28:51 +0300

    Option switch system added

--- thatsme/app/[me]/components/edit/edit.tsx
+++ thatsme/app/[me]/components/edit/edit.tsx
@@ -1,22 +1,25 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './page.module.css'
 
 import Toggle from './components/toggle/toggle'
 import Option from './components/option/option'
 
 export default function EditProfile() {
+
+    const [option, setOption] = useState<number>(1)
+
     return (
         <>
             <div className={styles.edit}>
                 <div className={styles.theedit}>
                     <h1 className={styles.title}><span className='purple'>ThatsMe</span> Settings</h1>
                     <div className={styles.options}>
-                        <Option title='All' />
-                        <Option title='General' />
-                        <Option title='Account' />
-                        <Option title='Connections' />
+                        <Option onClick={() => { setOption(1) }} backColor={option === 1 ? 'rgb(165, 107, 240)' : ''} title='All' />
+                        <Option onClick={() => { setOption(2) }} backColor={option === 2 ? 'rgb(165, 107, 240)' : ''} title='General' />
+                        <Option onClick={() => { setOption(3) }} backColor={option === 3 ? 'rgb(165, 107, 240)' : ''} title='Account' />
+                        <Option onClick={() => { setOption(4) }} backColor={option === 4 ? 'rgb(165, 107, 240)' : ''} title='Connections' />
                     </div>
 
                     <div className={styles.theoptions}>
