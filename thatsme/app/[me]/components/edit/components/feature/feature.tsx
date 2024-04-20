5cece654975d5877ae2187dfb219ef5141f81700
denis8sw <denisappleid12@gmail.com>
2024-04-20 20:40:36 +0300

    Feature component demo design

--- thatsme/app/[me]/components/edit/components/feature/feature.tsx
+++ thatsme/app/[me]/components/edit/components/feature/feature.tsx
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, { MouseEventHandler, ReactNode } from 'react'
 import styles from './page.module.css'
 
-export default function Feature() {
-  return (
-    <div className={styles.feature}>
+import { IoMdSettings } from "react-icons/io";
 
-    </div>
-  )
+interface FeatureProps {
+    children?: ReactNode;
+    title: string;
+    toggledBackColor?: string;
+    optionsClick?: MouseEventHandler<HTMLDivElement>;
+    optionsClickIcon?: MouseEventHandler<SVGElement>;
+    toggleClick?: MouseEventHandler<HTMLDivElement>;
+}
+
+export default function Feature({ children, title, optionsClick, optionsClickIcon, toggleClick, toggledBackColor}: FeatureProps) {
+
+    return (
+        <div className={styles.feature}>
+            <div className={styles.main}>
+                <span className={styles.icon}>{children}</span>
+                <h2 className={styles.title}>{title}</h2>
+            </div>
+            <div className={styles.options}>
+                <div onClick={optionsClick} className={styles.option}>
+                    <p>Options</p>
+                </div>
+                <div className={styles.optionicon}><IoMdSettings onClick={optionsClickIcon} /></div>
+            </div>
+            <div style={{backgroundColor: toggledBackColor}}  onClick={toggleClick} className={styles.toggle}>
+                <p className={styles.toggletext}>Enabled</p>
+            </div>
+        </div>
+    )
 }
