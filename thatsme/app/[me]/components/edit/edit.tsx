cb3028443f1d975396c1d82183f8011c4a4253e7
denis8sw <denisappleid12@gmail.com>
2024-04-20 15:05:08 +0300

    Creating the options & using the option component

--- thatsme/app/[me]/components/edit/edit.tsx
+++ thatsme/app/[me]/components/edit/edit.tsx
@@ -3,14 +3,18 @@
 import React from 'react'
 import styles from './page.module.css'
 
-import Toggle from './components/toggle'
+import Toggle from './components/toggle/toggle'
+import Option from './components/option/option'
 
 export default function EditProfile() {
     return (
         <>
             <div className={styles.edit}>
                 <div className={styles.theedit}>
-
+                    <h1 className={styles.title}><span className='purple'>ThatsMe</span> Settings</h1>
+                    <div className={styles.options}>
+                    <Option title='All'/>
+                    </div>
                 </div>
             </div>
         </>
