e498c9182adceda79651c759d565284d13b59e2d
denis8sw <denisappleid12@gmail.com>
2024-04-21 12:24:23 +0300

    Better structure & design for the features

--- thatsme/app/[me]/components/edit/edit.tsx
+++ thatsme/app/[me]/components/edit/edit.tsx
@@ -6,6 +6,8 @@ import styles from './page.module.css'
 import { useError } from '@/app/states/errorstate'
 import { useEditor } from '@/app/states/editor'
 
+import BackComponent from '../Pages/back/back'
+
 import Option from './components/option/option'
 import EditOptions from '../options/options'
 import FeaturesPages from '../features/features'
@@ -26,15 +28,24 @@ export default function EditProfile() {
                     <IoMdClose onClick={() => { toggleBoolean() }} className={styles.closeedit} />
                     <h1 className={styles.title}><span className='purple'>ThatsMe</span> Settings</h1>
                     <div className={styles.options}>
-                        <Option onClick={() => { setOption(1) }} backColor={option === 1 ? 'rgb(165, 107, 240)' : ''} title='All' />
-                        <Option onClick={() => { setOption(2) }} backColor={option === 2 ? 'rgb(165, 107, 240)' : ''} title='General' />
-                        <Option onClick={() => { setOption(3) }} backColor={option === 3 ? 'rgb(165, 107, 240)' : ''} title='Account' />
-                        <Option onClick={() => { setOption(4) }} backColor={option === 4 ? 'rgb(165, 107, 240)' : ''} title='Connections' />
+                        <Option onClick={() => { setOption(1); setPage('') }} backColor={option === 1 ? 'rgb(165, 107, 240)' : ''} title='All' />
+                        <Option onClick={() => { setOption(2); setPage('') }} backColor={option === 2 ? 'rgb(165, 107, 240)' : ''} title='General' />
+                        <Option onClick={() => { setOption(3); setPage('') }} backColor={option === 3 ? 'rgb(165, 107, 240)' : ''} title='Account' />
+                        <Option onClick={() => { setOption(4); setPage('') }} backColor={option === 4 ? 'rgb(165, 107, 240)' : ''} title='Connections' />
                     </div>
 
-                    <div className={styles.thefeatures}>
-                        <FeaturesPages />
+                    <div style={{display: page === '' ? '' : 'none'}} className={styles.thefeatures}>
                         <EditOptions />
+                    </div>
+
+                    <div style={{display: page === '' ? 'none' : ''}} className={styles.featurepages}>
+                        {page !== '' &&
+                            <div className={styles.back}>
+                                <BackComponent />
+                                <h2 className={styles.pagetitle}>{page} Settings</h2>
+                            </div>
+                        }
+                        <FeaturesPages />
                     </div>
                 </div>
             </div>
