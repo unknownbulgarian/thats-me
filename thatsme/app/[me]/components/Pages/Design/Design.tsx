37dad80d20abe98798201f98d5a97acc7d98cb9a
denis8sw <denisappleid12@gmail.com>
2024-04-21 22:02:29 +0300

    Update Design.tsx

--- thatsme/app/[me]/components/Pages/Design/Design.tsx
+++ thatsme/app/[me]/components/Pages/Design/Design.tsx
@@ -3,14 +3,19 @@
 import React, { useState } from 'react'
 import styles from './page.module.css'
 
+import { useConfig } from '@/app/states/user_config'
+
 import Toggler from '../components/toggler/toggler'
 
+
 export default function DesignPage() {
+
+  const {customBackground, toggleCustomBackground} = useConfig()
 
 
   return (
     <div className={styles.design}>
-      <Toggler toggled={true} title='Custom Background' />
+      <Toggler toggled={customBackground ? true : false} onClick={() => {toggleCustomBackground()}} title='Custom Background' />
     </div>
   )
 }
