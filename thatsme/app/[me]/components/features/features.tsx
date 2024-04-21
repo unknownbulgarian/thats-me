d7062d4852b27feecdb562606c168339c84a3493
denis8sw <denisappleid12@gmail.com>
2024-04-21 10:56:58 +0300

    Pages logic

--- thatsme/app/[me]/components/features/features.tsx
+++ thatsme/app/[me]/components/features/features.tsx
@@ -3,15 +3,17 @@ import React from 'react'
 import { useEditor } from '@/app/states/editor'
 import { useError } from '@/app/states/errorstate'
 
+import DesignPage from '../Pages/Design/Design'
+
 export default function FeaturesPages() {
 
 
     const { toggleBoolean, page, setPage, option, setOption } = useEditor()
     const { showError } = useError()
 
-  return (
-    <>
-
-    </>
-  )
+    return (
+        <>
+            {page === 'design' && <DesignPage />}
+        </>
+    )
 }
