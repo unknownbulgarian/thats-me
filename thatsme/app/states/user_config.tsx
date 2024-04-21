976297bda4716d4680f67c36c8a33d69d2eee2b0
denis8sw <denisappleid12@gmail.com>
2024-04-21 16:33:38 +0300

    Update user_config.tsx

--- thatsme/app/states/user_config.tsx
+++ thatsme/app/states/user_config.tsx
@@ -3,27 +3,68 @@
 import React, { createContext, useContext, useState, ReactNode } from 'react';
 
 interface UserConfigType {
-  
+    isConnections: boolean;
+    toggleConnections: () => void;
+    isMusic: boolean;
+    toggleMusic: () => void;
+    isParticles: boolean;
+    toggleParticles: () => void;
+    isFunction: boolean;
+    toggleFunction: () => void;
+    isAnimations: boolean;
+    toggleAnimations: () => void;
 }
 
 const UserConfigContext = createContext<UserConfigType | undefined>(undefined);
 
 export const useConfig = (): UserConfigType => {
-  const context = useContext(UserConfigContext);
-  if (!context) {
-    throw new Error('useConfig must be used within an EditorProvider');
-  }
-  return context;
+    const context = useContext(UserConfigContext);
+    if (!context) {
+        throw new Error('useConfig must be used within an EditorProvider');
+    }
+    return context;
 };
 
 export const UserConfigProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
 
+    const [isConnections, setConnections] = useState<boolean>(false)
+    const [isMusic, setMusic] = useState<boolean>(false)
+    const [isParticles, setParticles] = useState<boolean>(false)
+    const [isFunction, setFunction] = useState<boolean>(false)
+    const [isAnimations, setAnimations] = useState<boolean>(false)
 
+    const toggleConnections = () => {
+        setConnections(prevValue => !prevValue)
+    }
 
+    const toggleMusic = () => {
+        setMusic(prevValue => !prevValue)
+    }
 
-  const value = {
-    
-  };
+    const toggleParticles = () => {
+        setParticles(prevValue => !prevValue)
+    }
 
-  return <UserConfigContext.Provider value={value}>{children}</UserConfigContext.Provider>;
+    const toggleFunction = () => {
+        setFunction(prevValue => !prevValue)
+    }
+
+    const toggleAnimations = () => {
+        setAnimations(prevValue => !prevValue)
+    }
+
+    const value = {
+        isConnections,
+        toggleConnections,
+        isMusic,
+        toggleMusic,
+        isParticles,
+        toggleParticles,
+        isFunction,
+        toggleFunction,
+        isAnimations,
+        toggleAnimations
+    };
+
+    return <UserConfigContext.Provider value={value}>{children}</UserConfigContext.Provider>;
 };
