bc1ae7c483435ddde73f3a013bed48b1de486dcd
denis8sw <denisappleid12@gmail.com>
2024-04-22 16:36:36 +0300

    First Box Checker Component Demo

--- thatsme/app/[me]/components/Pages/components/boxchecker/boxchecker.tsx
+++ thatsme/app/[me]/components/Pages/components/boxchecker/boxchecker.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { MouseEventHandler, ReactNode } from 'react'
 import styles from './page.module.css'
 
+import { FaCheck } from "react-icons/fa";
 
-export default function boxchecker() {
-  return (
-    <div className={styles.boxchecker}>
+interface BoxCheckerProps {
+    title: string;
+    toggled: boolean;
+    chroma?: boolean;
+    onClick?: MouseEventHandler<HTMLDivElement>
+}
 
-    </div>
-  )
+
+export default function BoxChecker({ title, onClick, toggled, chroma }: BoxCheckerProps) {
+    return (
+        <div className={styles.boxchecker}>
+            <div onClick={onClick} className={styles.box}>
+                {toggled && <FaCheck className={styles.icon} />}
+            </div>
+            <p  className={`${chroma ? styles.chroma : styles.title}`}>{title}</p>
+        </div>
+    )
 }
