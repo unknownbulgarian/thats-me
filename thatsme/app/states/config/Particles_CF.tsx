6a388b3a031788542ae10cccf3e5d11956269b9f
denis8sw <denisappleid12@gmail.com>
2024-04-23 17:28:04 +0300

    Update Particles_CF.tsx

--- thatsme/app/states/config/Particles_CF.tsx
+++ thatsme/app/states/config/Particles_CF.tsx
@@ -1,10 +1,22 @@
 'use client';
 
-import React, { createContext, useContext, useState, ReactNode } from 'react';
+import React, { createContext, useContext, useState, ReactNode, Dispatch, SetStateAction } from 'react';
 
 interface UserConfigType {
     particles: boolean;
     toggleParticles: () => void;
+
+    //general settings
+    particlesNumber: number;
+    setParticlesNumber: Dispatch<SetStateAction<number>>
+
+    particlesOpacity: number;
+    setParticlesOpacity: Dispatch<SetStateAction<number>>;
+
+    hover: boolean;
+    setHover: Dispatch<SetStateAction<boolean>>
+    
+
 }
 
 const UserConfigContext = createContext<UserConfigType | undefined>(undefined);
@@ -19,15 +31,40 @@ export const useParticlesConfig = (): UserConfigType => {
 
 export const UserParticlesProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
 
-    const [particles, setParticles] = useState<boolean>(false)
+    const [particles, setParticles] = useState<boolean>(true)
 
     const toggleParticles = () => {
         setParticles(prevValue => !prevValue)
     }
 
+
+    //details config starts from here
+
+    //general
+    const [particlesNumber, setParticlesNumber] = useState<number>(80)
+    const [particlesOpacity, setParticlesOpacity] = useState<number>(0.5)
+
+
+    const [hover, setHover] = useState<boolean>(false)
+
+
+
+
+
+
     const value = {
-      particles,
-      toggleParticles
+        particles,
+        toggleParticles,
+
+        //general settings
+        particlesNumber,
+        setParticlesNumber,
+
+        particlesOpacity,
+        setParticlesOpacity,
+
+        hover,
+        setHover,
     };
 
     return <UserConfigContext.Provider value={value}>{children}</UserConfigContext.Provider>;
