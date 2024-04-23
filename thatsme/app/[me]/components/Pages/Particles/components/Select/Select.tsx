fcac3b89f616ba12fbfaabe10e74a9a1435bc78a
denis8sw <denisappleid12@gmail.com>
2024-04-23 21:59:40 +0300

    Update Select.tsx

--- thatsme/app/[me]/components/Pages/Particles/components/Select/Select.tsx
+++ thatsme/app/[me]/components/Pages/Particles/components/Select/Select.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Dispatch, MouseEventHandler, SetStateAction } from 'react'
 import styles from './page.module.css'
 
 interface TitleObject {
@@ -7,15 +7,35 @@ interface TitleObject {
 
 interface SelectProps {
     titles?: Array<TitleObject>
+    title: string;
+    expand: boolean;
+    height?: string;
+    onClick: MouseEventHandler<HTMLDivElement>
+    noClick?: MouseEventHandler<HTMLDivElement>
+    setExpand: Dispatch<SetStateAction<boolean>>;  
+    setOption: Dispatch<SetStateAction<string>>;  
+    value?: string;
 }
 
 
-export default function Select({ titles }: SelectProps) {
+export default function Select({ titles, title, expand, onClick, height, value, noClick, setOption, setExpand }: SelectProps) {
+
+
+
     return (
-        <div className={styles.select}>
-            {titles?.map((item, index) => (
-                <></>
-            ))}
-        </div>
+        <>
+            <p className={styles.title}>{title}</p>
+            <div onClick={!expand ? onClick : noClick} className={styles.select}
+                style={{
+                    height: expand ? height : '25px',
+                    overflow: expand ? 'auto' : 'hidden'
+                }}
+            >
+                {!expand && <p className={styles.value}>{value}</p>}
+                {expand && titles?.map((item, index) => (
+                    <p onClick={() => {setExpand(false); setOption(item.title) }} className={styles.option}>{item.title}</p>
+                ))}
+            </div>
+        </>
     )
 }
