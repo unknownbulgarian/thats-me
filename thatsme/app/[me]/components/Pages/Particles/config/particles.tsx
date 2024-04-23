b50ba6fa45b5c56e595595bced297ae73892560b
denis8sw <denisappleid12@gmail.com>
2024-04-23 12:50:36 +0300

    Particles generating system

--- thatsme/app/[me]/components/Pages/Particles/config/particles.tsx
+++ thatsme/app/[me]/components/Pages/Particles/config/particles.tsx
@@ -1,12 +1,63 @@
-import React from 'react'
+'use client'
+
+
+import React, { useEffect, useRef, useState } from 'react'
 import styles from './page.module.css'
 
 import { useParticlesConfig } from '@/app/states/config/Particles_CF'
 
-export default function particles() {
-  return (
-    <div className={styles.particles}>
-        
-    </div>
-  )
+export default function Particles() {
+
+    const { particles } = useParticlesConfig()
+
+    const [particlesNum, setParticlesNum] = useState<Array<number>>([]);
+
+    const intervalRef = useRef<NodeJS.Timeout | null>(null);
+    const removeTimeoutRef = useRef<NodeJS.Timeout | null>(null);
+
+    const generateParticle = () => {
+        const randomNumber = Math.floor(Math.random() * 1000000);
+        setParticlesNum((prevArray) => [...prevArray, randomNumber]);
+        removeFirstParticle()
+    };
+
+    const removeFirstParticle = () => {
+        setParticlesNum((prevArray) => {
+            if (prevArray.length > 10) {
+                return prevArray.slice(5);
+            } else {
+                return prevArray;
+            }
+        });
+    };
+
+    useEffect(() => {
+        intervalRef.current = setInterval(generateParticle, 1000);
+
+
+        return () => {
+
+            if (intervalRef.current) {
+                clearInterval(intervalRef.current);
+            }
+        };
+    }, []);
+
+
+
+    return (
+        <>
+            {particles &&
+                <div className={styles.particles}>
+                    {particlesNum.map((index) => (
+                        <div
+                            key={index}
+                            className={styles.particle}>
+
+                        </div>
+                    ))}
+                </div>
+            }
+        </>
+    )
 }
