f40863c349f167e40ba35edec4d40f3404644ced
denis8sw <denisappleid12@gmail.com>
2024-04-23 12:22:14 +0300

    Creating the particles

---
+++ thatsme/app/[me]/components/Pages/Particles/config/particles.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './page.module.css'
+
+import { useParticlesConfig } from '@/app/states/config/Particles_CF'
+
+export default function particles() {
+  return (
+    <div className={styles.particles}>
+        
+    </div>
+  )
+}
