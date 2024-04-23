7188aa7a069b7d20784b3de4b18c395e373b7cef
denis8sw <denisappleid12@gmail.com>
2024-04-23 12:22:02 +0300

    Adding the particles page

--- thatsme/app/[me]/components/features/features.tsx
+++ thatsme/app/[me]/components/features/features.tsx
@@ -4,18 +4,20 @@ import { useEditor } from '@/app/states/editor'
 import { useError } from '@/app/states/errorstate'
 
 import DesignPage from '../Pages/Design/Design'
-import Connections from '../Pages/Connections/Connections'
+import ConnectionsPage from '../Pages/Connections/Connections'
+import ParticlesPage from '../Pages/Particles/Particles'
 
 export default function FeaturesPages() {
 
 
-    const { toggleBoolean, page, setPage, option, setOption } = useEditor()
+    const { page } = useEditor()
     const { showError } = useError()
 
     return (
         <>
             {page === 'design' && <DesignPage />}
-            {page === 'connections' && <Connections />}
+            {page === 'connections' && <ConnectionsPage />}
+            {page === 'particles' && <ParticlesPage />}
         </>
     )
 }
