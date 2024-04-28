d5368ef582e3f6c74e1a7692f71376552d1ae406
TosuSW <denisappleid12@gmail.com>
2024-04-28 10:54:40 +0300

    Added more types for the RGInput component

--- thatsme/app/[me]/components/Pages/Particles/components/RGInput/RGInput.tsx
+++ thatsme/app/[me]/components/Pages/Particles/components/RGInput/RGInput.tsx
@@ -3,24 +3,27 @@ import styles from './style.module.css'
 
 interface RGInputProps {
     title: string;
-    value?: number;
+    value?: number | string;
     onInput?: FormEventHandler<HTMLInputElement>;
-    max: number;
-    min: number;
-    maxLength: number;
+    max?: number;
+    min?: number;
+    maxLength?: number;
     marginBottom?: string;
+    inputWidth?: string;
+    height?: string;
+    type: string;
 }
 
-export default function RGInput({ title, value, onInput, max, min, maxLength, marginBottom }: RGInputProps) {
+export default function RGInput({ title, value, onInput, max, min, maxLength, marginBottom, inputWidth, height, type }: RGInputProps) {
     return (
         <div style={{marginBottom: marginBottom}} className={styles.rg}>
             <div className={styles.main}>
                 <p className={styles.title}>{title}</p>
-                <input spellCheck='false' autoComplete='off' 
-                type='number' maxLength={maxLength} onInput={onInput} value={value} className={styles.boxinp}></input>
+                <input  spellCheck='false' autoComplete='off' 
+                type={type === 'range' ? 'number' : 'text'} maxLength={maxLength} onInput={onInput} value={value} className={styles.boxinp}></input>
             </div>
-            <input spellCheck='false' autoComplete='off' 
-            max={max} min={min} className={styles.theinp} onInput={onInput} value={value} type='range'></input>
+            <input style={{width: inputWidth, height: height}} spellCheck='false' autoComplete='off' 
+            max={max} min={min} className={styles.theinp} onInput={onInput} value={value} type={type}></input>
         </div>
     )
 }
