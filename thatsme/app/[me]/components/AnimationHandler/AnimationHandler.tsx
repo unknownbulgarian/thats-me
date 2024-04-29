2de40858a338ae0cffa253a8ae617f8aa85bccd8
TosuSW <denisappleid12@gmail.com>
2024-04-29 09:54:51 +0300

    Creating animation handler div
    - This will allow me to still keep the server side part

---
+++ thatsme/app/[me]/components/AnimationHandler/AnimationHandler.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import React from 'react'
+
+interface AnimationHandlerProps {
+    children: React.ReactNode
+}
+
+export default function AnimationHandler({ children }: AnimationHandlerProps) {
+
+
+
+
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
