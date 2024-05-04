eba2e21c807958bed4248b45f7275d9902f1910c
TosuSW <denisappleid12@gmail.com>
2024-05-04 10:06:22 +0300

    Update AnimationHandler.tsx

--- thatsme/app/[me]/components/AnimationHandler/AnimationHandler.tsx
+++ thatsme/app/[me]/components/AnimationHandler/AnimationHandler.tsx
@@ -1,19 +1,43 @@
 'use client'
 
 import React from 'react'
+import '../../../animations.css'
+
+
+import { useAnimationsConfig } from '@/app/states/config/Animations_CF'
 
 interface AnimationHandlerProps {
     children: React.ReactNode
+    gap?: string;
+    display?: string;
+    isSocials?: boolean;
 }
 
-export default function AnimationHandler({ children }: AnimationHandlerProps) {
+export default function AnimationHandler({ children, gap, isSocials, display }: AnimationHandlerProps) {
 
-
+    const { animation } = useAnimationsConfig()
 
 
     return (
-        <div>
-            {children}
-        </div>
+        <>
+            {animation === "slide-right" &&
+                <div style={{gap: isSocials ? gap : '', display: isSocials ? 'flex' : ''}} className='slide-right'>
+                    {children}
+                </div>}
+
+            {animation === "slide-left" &&
+                <div style={{gap: isSocials ? gap : '', display: isSocials ? 'flex' : ''}}className='slide-left'>
+                    {children}
+                </div>}
+
+                {animation === "slide-top" &&
+                <div style={{gap: isSocials ? gap : '', display: isSocials ? 'flex' : ''}} className='slide-top'>
+                    {children}
+                </div>}
+                {animation === "slide-bottom" &&
+                <div style={{gap: isSocials ? gap : '', display: isSocials ? 'flex' : ''}} className='slide-bottom'>
+                    {children}
+                </div>}
+        </>
     )
 }
