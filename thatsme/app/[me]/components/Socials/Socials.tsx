dfbe2b5e989e304b197d53c08eb1ecb0c359929f
TosuSW <denisappleid12@gmail.com>
2024-05-08 15:45:52 +0300

    Socials logic created

--- thatsme/app/[me]/components/Socials/Socials.tsx
+++ thatsme/app/[me]/components/Socials/Socials.tsx
@@ -11,21 +11,40 @@ import { FaSpotify } from "react-icons/fa";
 import AnimationHandler from '../AnimationHandler/AnimationHandler';
 
 import { useConnectionsConfig } from '@/app/states/config/Connections_CF';
-
+import { useConfig } from '@/app/states/config/Main&&Design';
 
 
 export default function Socials() {
 
-    const { instagram, steam, spotify, facebook } = useConnectionsConfig()
+    const { isConnections } = useConfig()
+
+    const {
+        instagram, steam, spotify, facebook,
+        instagramUrl, steamUrl, spotifyUrl, facebookUrl
+
+    } = useConnectionsConfig()
+
+    const inURL = 'https://www.instagram.com/'
+    const stURL = 'https://steamcommunity.com/'
+    const spURL = 'https://open.spotify.com/'
+    const fcURL = 'https://facebook.com/'
+
+    const openConnection = (defaultUrl: string, name: string) => {
+        window.open(defaultUrl + name, '_blank')
+    }
 
     return (
-        <div className={styles.links}>
-            <AnimationHandler isSocials={true} gap='1em'>
-                {instagram && <FaInstagram className={styles.linkicon} />}
-                {steam && <FaSteam className={styles.linkicon} />}
-                {spotify && <FaSpotify className={styles.linkicon} />}
-                {facebook && <FaFacebookF className={styles.linkicon} />}
-            </AnimationHandler>
-        </div>
+        <>
+            {isConnections &&
+                <div className={styles.links}>
+                    <AnimationHandler isSocials={true} gap='1em'>
+                        {instagram && <FaInstagram className={styles.linkicon} onClick={() => { openConnection(inURL, instagramUrl) }} />}
+                        {steam && <FaSteam className={styles.linkicon} onClick={() => { openConnection(stURL, steamUrl) }} />}
+                        {spotify && <FaSpotify className={styles.linkicon} onClick={() => { openConnection(spURL, spotifyUrl) }} />}
+                        {facebook && <FaFacebookF className={styles.linkicon} onClick={() => { openConnection(fcURL, facebookUrl) }} />}
+                    </AnimationHandler>
+                </div>
+            }
+        </>
     )
 }
