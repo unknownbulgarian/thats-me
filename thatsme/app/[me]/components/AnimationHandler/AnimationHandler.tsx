3dda1e6aeb43029f1fb45e3ec9ece55bf500954f
TosuSW <denisappleid12@gmail.com>
2024-05-08 15:53:46 +0300

    Update AnimationHandler.tsx

--- thatsme/app/[me]/components/AnimationHandler/AnimationHandler.tsx
+++ thatsme/app/[me]/components/AnimationHandler/AnimationHandler.tsx
@@ -5,6 +5,7 @@ import '../../../animations.css'
 
 
 import { useAnimationsConfig } from '@/app/states/config/Animations_CF'
+import { useConfig } from '@/app/states/config/Main&&Design'
 
 interface AnimationHandlerProps {
     children: React.ReactNode
@@ -16,28 +17,29 @@ interface AnimationHandlerProps {
 export default function AnimationHandler({ children, gap, isSocials, display }: AnimationHandlerProps) {
 
     const { animation } = useAnimationsConfig()
+    const { isAnimations } = useConfig()
 
 
     return (
         <>
-            {animation === "slide-right" &&
-                <div style={{gap: isSocials ? gap : '', display: isSocials ? 'flex' : ''}} className='slide-right'>
-                    {children}
-                </div>}
+                    {animation === "slide-right" &&
+                        <div style={{ gap: isSocials ? gap : '', display: isSocials ? 'flex' : '' }} className={isAnimations ? 'slide-right' : ''}>
+                            {children}
+                        </div>}
 
-            {animation === "slide-left" &&
-                <div style={{gap: isSocials ? gap : '', display: isSocials ? 'flex' : ''}}className='slide-left'>
-                    {children}
-                </div>}
+                    {animation === "slide-left" &&
+                        <div style={{ gap: isSocials ? gap : '', display: isSocials ? 'flex' : '' }} className={isAnimations ? 'slide-left' : ''}>
+                            {children}
+                        </div>}
 
-                {animation === "slide-top" &&
-                <div style={{gap: isSocials ? gap : '', display: isSocials ? 'flex' : ''}} className='slide-top'>
-                    {children}
-                </div>}
-                {animation === "slide-bottom" &&
-                <div style={{gap: isSocials ? gap : '', display: isSocials ? 'flex' : ''}} className='slide-bottom'>
-                    {children}
-                </div>}
+                    {animation === "slide-top" &&
+                        <div style={{ gap: isSocials ? gap : '', display: isSocials ? 'flex' : '' }} className={isAnimations ? 'slide-top' : ''}>
+                            {children}
+                        </div>}
+                    {animation === "slide-bottom" &&
+                        <div style={{ gap: isSocials ? gap : '', display: isSocials ? 'flex' : '' }} className={isAnimations ? 'slide-bottom' : ''}>
+                            {children}
+                        </div>}
         </>
     )
 }
