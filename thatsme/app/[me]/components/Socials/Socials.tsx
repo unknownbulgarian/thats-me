65b3e5ccb010ef3b119837df71e10267b3975238
TosuSW <denisappleid12@gmail.com>
2024-05-08 15:32:18 +0300

    Added the logic for the socials

--- thatsme/app/[me]/components/Socials/Socials.tsx
+++ thatsme/app/[me]/components/Socials/Socials.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from 'react'
 
 import styles from './page.module.css'
@@ -8,14 +10,21 @@ import { FaSteam } from "react-icons/fa6";
 import { FaSpotify } from "react-icons/fa";
 import AnimationHandler from '../AnimationHandler/AnimationHandler';
 
+import { useConnectionsConfig } from '@/app/states/config/Connections_CF';
+
+
+
 export default function Socials() {
+
+    const { instagram, steam, spotify, facebook } = useConnectionsConfig()
+
     return (
         <div className={styles.links}>
             <AnimationHandler isSocials={true} gap='1em'>
-                <FaInstagram className={styles.linkicon} />
-                <FaSteam className={styles.linkicon} />
-                <FaSpotify className={styles.linkicon} />
-                <FaFacebookF className={styles.linkicon} />
+                {instagram && <FaInstagram className={styles.linkicon} />}
+                {steam && <FaSteam className={styles.linkicon} />}
+                {spotify && <FaSpotify className={styles.linkicon} />}
+                {facebook && <FaFacebookF className={styles.linkicon} />}
             </AnimationHandler>
         </div>
     )
