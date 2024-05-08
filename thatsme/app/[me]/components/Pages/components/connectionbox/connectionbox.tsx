b13882d3c88f2c6d63e34cb66ce9a5a8d370f2ce
TosuSW <denisappleid12@gmail.com>
2024-05-08 15:37:50 +0300

    Update connectionbox.tsx

--- thatsme/app/[me]/components/Pages/components/connectionbox/connectionbox.tsx
+++ thatsme/app/[me]/components/Pages/components/connectionbox/connectionbox.tsx
@@ -1,19 +1,20 @@
-import React from 'react'
+import React, { FormEventHandler } from 'react'
 import styles from './page.module.css'
 
 interface ConnectionBoxProps{
     title: string;
     placeholder?:string;
+    onInput?: FormEventHandler<HTMLInputElement> | undefined
 }
 
 
-export default function ConnectionBox({title, placeholder} : ConnectionBoxProps) {
+export default function ConnectionBox({title, placeholder, onInput} : ConnectionBoxProps) {
     return (
         <div className={styles.box}>
             <div className={styles.link}>
                 <p className={styles.title}>{title}</p>
             </div>
-            <input autoComplete='off' spellCheck='false' className={styles.theinput} placeholder={placeholder}></input>
+            <input onInput={onInput} autoComplete='off' spellCheck='false' className={styles.theinput} placeholder={placeholder}></input>
         </div>
     )
 }
