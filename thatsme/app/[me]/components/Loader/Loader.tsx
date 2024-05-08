e74824c95dd9a7d0f488d0dcebec9fe3562307dd
TosuSW <denisappleid12@gmail.com>
2024-05-08 17:29:05 +0300

    Loader demo

--- thatsme/app/[me]/components/Loader/Loader.tsx
+++ thatsme/app/[me]/components/Loader/Loader.tsx
@@ -1,14 +1,67 @@
 'use client'
 
-import React from 'react'
+import React, { useEffect } from 'react'
 
-const loadSettings = async () => {
-  
+import { useApiUrl } from '@/app/states/api'
+
+//features
+import { useConfig } from '@/app/states/config/Main&&Design'
+
+interface ConfigProps {
+    params: { me: string };
 }
 
 
-export default function LoaderConfig() {
+const LoaderConfig: React.FC<ConfigProps> = ({ params }) => {
+
+    const apiUrl = useApiUrl()
+    const {
+        setBackgroundColor, setBackgroundChroma,
+        
+    } = useConfig()
+
+
+    const loadSettings = async () => {
+        try {
+            const response = await fetch(apiUrl + '/loadSettings', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ username: params.me })
+            });
+
+            const data = await response.json();
+
+            if (!data.error) {
+                console.log(data.settings)
+                const settings = data.settings
+
+                const design = settings.designSettings
+
+                setBackgroundChroma(design.backgroundColor.chroma)
+                setBackgroundColor(design.backgroundColor.color)
+
+            } else {
+                console.log(data.error)
+            }
+
+            return data;
+        } catch (error) {
+            console.error('Error fetching features:', error);
+            throw error;
+        }
+    }
+
+    useEffect(() => {
+        loadSettings()
+    }, [])
+
+
     return (
         <></>
     )
 }
+
+
+export default LoaderConfig
