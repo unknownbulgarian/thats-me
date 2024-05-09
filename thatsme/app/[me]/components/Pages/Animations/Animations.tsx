280619db80182a9695f15c2428e1627dbea3103d
TosuSW <denisappleid12@gmail.com>
2024-05-09 22:50:36 +0300

    Saving logic animations

--- thatsme/app/[me]/components/Pages/Animations/Animations.tsx
+++ thatsme/app/[me]/components/Pages/Animations/Animations.tsx
@@ -5,16 +5,22 @@ import styles from './page.module.css'
 
 import { useAnimationsConfig } from '@/app/states/config/Animations_CF'
 
-import Toggler from '../components/toggler/toggler'
-import Select from '../Particles/components/Select/Select'
-import BoxChecker from '../components/boxchecker/boxchecker'
 
+import Select from '../Particles/components/Select/Select'
+import ButtonIcon from '@/app/global-components/button-icon/buttonicon'
+import { useApiUrl } from '@/app/states/api'
+import { useError } from '@/app/states/errorstate'
+
+import { FaSave } from "react-icons/fa";
 
 interface titleObject {
     title: string;
 }
 
 export default function AnimationsPage() {
+
+    const apiUrl = useApiUrl()
+    const { showError, showSuccess } = useError()
 
     const [animationExpand, setAnimationExpand] = useState<boolean>(false)
 
@@ -29,13 +35,54 @@ export default function AnimationsPage() {
         { title: 'slide-bottom' },
     ]
 
+    const saveAnimation = async () => {
+        try {
+            const response = await fetch(apiUrl + '/saveAnimation', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ animation, token: localStorage.getItem('token') })
+            });
 
+            const data = await response.json();
+
+            if (!data.error) {
+                showSuccess('Saved successfully')
+            } else {
+                showError('Something went wrong')
+            }
+
+            return data;
+        } catch (error) {
+            console.error('Error fetching features:', error);
+            throw error;
+        }
+
+    }
 
 
 
     return (
         <div className={styles.animations}>
             <Select height='85px' value={animation} titles={animations} setOption={setAnimation} title='Animation' expand={animationExpand ? true : false} setExpand={setAnimationExpand} onClick={() => { setAnimationExpand(true) }} />
+            <ButtonIcon
+                background="linear-gradient(to right, #470c7e, #4e1187, #551690, #5d1b99, #6420a2, #6524a6, #6728ab, #682caf, #6330ae, #5f33ae, #5a35ad, #5638ac)"
+                borderRadius="0.3em"
+                width="80px"
+                height="25px"
+                color="white"
+                title="Save"
+                titleColor="white"
+                iconFontSize="0.8rem"
+                titleFontSize='0.8rem'
+                marginTop='0.8em'
+                transform={false}
+                buttonType='submit'
+                onClick={() => { saveAnimation() }}
+            >
+                <FaSave />
+            </ButtonIcon>
         </div>
     )
 }
