ec1efd66e858c5476baae68509cfe53667e9930e
TosuSW <denisappleid12@gmail.com>
2024-05-09 11:50:56 +0300

    Username component config

--- thatsme/app/[me]/components/config/Design/Username/Username.tsx
+++ thatsme/app/[me]/components/config/Design/Username/Username.tsx
@@ -1,13 +1,31 @@
+'use client'
+
+
 import React from 'react'
 import styles from './page.module.css'
+
+import { useConfig } from '@/app/states/config/Main&&Design';
+
+
 
 interface UsernameProps {
     params: { me: string };
 }
 
 
-export default function Username({params} : UsernameProps) {
-  return (
-    <h1 className={styles.username}>{params.me}</h1>
-  )
+export default function Username({ params }: UsernameProps) {
+
+    const { customUsernameColor, theUsernameColor, usernameChroma } = useConfig()
+
+
+    return (
+        <>
+            {customUsernameColor
+                ?
+                <h1 style={{color: usernameChroma ? '' : theUsernameColor}} className={`${usernameChroma ? styles.username2 : styles.username}`}>{params.me}</h1>
+                :
+                <h1 className={styles.username}>{params.me}</h1>
+            }
+        </>
+    )
 }
